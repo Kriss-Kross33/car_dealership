2f2639f992055b183a093f0305e2f649f0d918b3
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-06 12:53:31 +0000

    config go_router for routing in app

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:car_dealership/features/splash/presentation/presentation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'route_consts.dart';
+
+class AppRouter {
+  static GoRouter router = GoRouter(
+    routes: <RouteBase>[
+      //* Splash
+      GoRoute(
+        path: RouteConsts.splashRoute,
+        pageBuilder: (context, state) {
+          return const MaterialPage(
+            child: SplashScreen(),
+          );
+        },
+      ),
+    ],
+  );
+}
---
+++ lib/core/routes/route_consts.dart
@@ -0,0 +1,10 @@
+class RouteConsts {
+  RouteConsts._();
+
+  static const String splashRoute = '/';
+  static const String walkthroughRoute = '/walkthrough';
+  static const String loginRoute = '/login';
+  static const String signupRoute = '/signup';
+  static const String homeRoute = '/home';
+  static const String settingsRoute = '/settings';
+}
