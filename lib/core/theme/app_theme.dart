f972d0a11bc6161aff172b3196dff8b0b457d22a
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-06 13:26:34 +0000

    add font assets

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,14 +2,20 @@ import 'package:car_dealership/core/core.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static const fontFamily = 'AirbnbCereal';
   static ThemeData get light {
     return ThemeData(
+      fontFamily: fontFamily,
       brightness: Brightness.light,
       primaryColor: ColorConsts.primaryColor,
     );
   }
 
   static ThemeData get dark {
-    return ThemeData(brightness: Brightness.dark);
+    return ThemeData(
+      fontFamily: fontFamily,
+      brightness: Brightness.dark,
+      primaryColor: ColorConsts.primaryColor,
+    );
   }
 }
