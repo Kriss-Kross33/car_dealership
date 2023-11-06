552b77c2e2e3d7999715e1b927725c353c42c557
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-06 13:28:26 +0000

    config theme for `MaterialApp`

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:car_dealership/core/core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      debugShowCheckedModeBanner: false,
+      routerDelegate: AppRouter.router.routerDelegate,
+      routeInformationParser: AppRouter.router.routeInformationParser,
+      routeInformationProvider: AppRouter.router.routeInformationProvider,
     );
   }
 }
