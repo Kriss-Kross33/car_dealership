aa4bb822620d22d3e6a3cc2bbd1d302b25f735a5
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-06 13:28:50 +0000

    add walkthrough paths to `AssetConsts`

--- lib/core/constants/asset_consts.dart
+++ lib/core/constants/asset_consts.dart
@@ -1,4 +1,8 @@
 class AssetConsts {
   static const String logo = 'assets/images/logo.png';
   static const String logo2 = 'assets/images/logo2.png';
+  static const String walkthroughCar1 = 'assets/images/walkthrough_car_1.png';
+  static const String walkthroughCar2 = 'assets/images/walkthrough_car_2.png';
+  static const String walkthroughCar3 = 'assets/images/walkthrough_car_3.png';
+  static const String walkthroughCar4 = 'assets/images/walkthrough_car_4.png';
 }
