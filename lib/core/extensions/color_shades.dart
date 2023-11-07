25abe5468db50cd3f1dff751e56603251b06c72e
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-07 10:05:11 +0000

    create extension for color shades

---
+++ lib/core/extensions/color_shades.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+extension on Color {
+  Color brighten([int percent = 10]) {
+    assert(
+      1 <= percent && percent <= 100,
+      'percentage must be between 1 and 100',
+    );
+    final p = percent / 100;
+    return Color.fromARGB(
+      alpha,
+      red + ((255 - red) * p).round(),
+      green + ((255 - green) * p).round(),
+      blue + ((255 - blue) * p).round(),
+    );
+  }
+}
