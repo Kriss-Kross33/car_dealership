2e1ae3fd6f7d43a4f2d62575ea98373835a6503b
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-07 10:07:29 +0000

    add white and black color codes

--- lib/core/theme/color_consts.dart
+++ lib/core/theme/color_consts.dart
@@ -4,4 +4,6 @@ class ColorConsts {
   ColorConsts._();
 
   static const Color primaryColor = Color(0xFFE54A4B);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
 }
