c0d587a4bbfe17249ce04be7ab0701614fbd3274
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-13 17:15:42 +0000

    initialize firebase before calling

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:car_dealership/firebase_options.dart';
+import 'package:car_dealership/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'src/core/core.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      debugShowCheckedModeBanner: false,
-      routerDelegate: AppRouter.router.routerDelegate,
-      routeInformationParser: AppRouter.router.routeInformationParser,
-      routeInformationProvider: AppRouter.router.routeInformationProvider,
-    );
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'kap-car-dealership',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  AuthenticationRepository authenticationRepository =
+      AuthenticationRepository();
+  runApp(
+    App(
+      authenticationRepository: authenticationRepository,
+    ),
+  );
 }
