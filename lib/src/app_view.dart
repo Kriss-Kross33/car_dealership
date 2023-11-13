4ca95e7a737a65146ba96be37ea098b4dc8ef2b0
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-13 17:17:29 +0000

    create separate widget in a new file for `MaterialApp`

---
+++ lib/src/app_view.dart
@@ -0,0 +1,18 @@
+import 'package:car_dealership/src/core/core.dart';
+import 'package:flutter/material.dart';
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      debugShowCheckedModeBanner: false,
+      routerDelegate: AppRouter.router.routerDelegate,
+      routeInformationParser: AppRouter.router.routeInformationParser,
+      routeInformationProvider: AppRouter.router.routeInformationProvider,
+    );
+  }
+}
