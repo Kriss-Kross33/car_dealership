ea05f6da545f9e93ad4638b6ec969355de24b86e
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-13 17:16:40 +0000

    move `MyApp` to seperate file and rename to `App`.

---
+++ lib/src/app.dart
@@ -0,0 +1,21 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:car_dealership/src/app_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+    required AuthenticationRepository authenticationRepository,
+  }) : _authenticationRepository = authenticationRepository;
+
+  final AuthenticationRepository _authenticationRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: const AppView(),
+    );
+  }
+}
