2a368f773079aea79e443d938a82622c04f35a22
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-21 19:26:04 +0000

    add new colors to `ColorConsts` class

--- lib/src/core/theme/color_consts.dart
+++ lib/src/core/theme/color_consts.dart
@@ -6,4 +6,6 @@ class ColorConsts {
   static const Color primaryColor = Color(0xFFE54A4B);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color green = Color(0xFF4AA061);
+  static const Color gold = Color(0xFFFFD700);
 }
