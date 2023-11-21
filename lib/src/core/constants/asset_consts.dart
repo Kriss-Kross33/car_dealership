7882d9d8d532f4aad78989bb2ca3f6daf36095e8
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-21 19:19:52 +0000

    add new image path to `AssetConsts`

--- lib/src/core/constants/asset_consts.dart
+++ lib/src/core/constants/asset_consts.dart
@@ -1,8 +1,42 @@
 class AssetConsts {
+  //* Icons
+  static const String notification = 'assets/images/notifications.png';
+  static const String filter2 = 'assets/images/filter2.png';
+  static const String search = 'assets/images/search.png';
+  static const String home = 'assets/images/home.png';
+  static const String fav = 'assets/images/fav.png';
+  static const String star = 'assets/images/star.png';
+  static const String car = 'assets/images/car.png';
+
+  static const String location = 'assets/images/location.png';
+  static const String gasoline = 'assets/images/gasoline.png';
+  static const String repair = 'assets/images/repair.png';
+  static const String rv = 'assets/images/rv.png';
+  static const String sedan = 'assets/images/sedan.png';
+  static const String tyre = 'assets/images/tyre.png';
+  static const String calendar = 'assets/images/calendar.png';
+  static const String engine = 'assets/images/engine.png';
+  static const String carRepair = 'assets/images/car_repair.png';
+  static const String ev = 'assets/images/ev.png';
+  static const String speedometer = 'assets/images/speedometer.png';
+
+  static const String spanner2 = 'assets/images/spanner2.png';
+
+  static const String person = 'assets/images/person.png';
+  static const String profile = 'assets/images/profile.png';
+  static const String mail = 'assets/images/mail.png';
+  static const String message = 'assets/images/message.png';
+
   static const String logo = 'assets/images/logo.png';
   static const String logo2 = 'assets/images/logo2.png';
   static const String walkthroughCar1 = 'assets/images/walkthrough_car_1.png';
   static const String walkthroughCar2 = 'assets/images/walkthrough_car_2.png';
   static const String walkthroughCar3 = 'assets/images/walkthrough_car_3.png';
   static const String walkthroughCar4 = 'assets/images/walkthrough_car_4.png';
+  static const String transBadge = 'assets/images/trans_badge.png';
+
+  static const String car1 = 'assets/images/car1.png';
+  static const String car2 = 'assets/images/car2.png';
+  static const String car3 = 'assets/images/car3.png';
+  static const String car4 = 'assets/images/car4.png';
 }
