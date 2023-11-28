73978e8381a4e20c63212becd2b04f42b383550d
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-28 13:24:05 +0000

    move logo to logos directory

--- lib/src/core/constants/asset_consts.dart
+++ lib/src/core/constants/asset_consts.dart
@@ -28,8 +28,8 @@ class AssetConsts {
   static const String mail = 'assets/images/mail.png';
   static const String message = 'assets/images/message.png';
 
-  static const String logo = 'assets/images/logo.png';
-  static const String logo2 = 'assets/images/logo2.png';
+  static const String logo = 'assets/logos/logo.png';
+  static const String logo2 = 'assets/logos/logo2.png';
   static const String walkthroughCar1 = 'assets/images/walkthrough_car_1.png';
   static const String walkthroughCar2 = 'assets/images/walkthrough_car_2.png';
   static const String walkthroughCar3 = 'assets/images/walkthrough_car_3.png';
