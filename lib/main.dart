d39a24f6e0eee205cf014a05e8f2a2cfc71a9980
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-11-30 13:03:12 +0000

    pass car_repository down the widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:car_dealership/firebase_options.dart';
 import 'package:car_dealership/src/app.dart';
+import 'package:car_remote_api/car_remote_api.dart';
+import 'package:car_repository/car_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [SystemUiOverlay.bottom],
+  );
   await Firebase.initializeApp(
     name: 'kap-car-dealership',
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   AuthenticationRepository authenticationRepository =
       AuthenticationRepository();
+  final carApi = CarRemoteApi();
+  CarRepository carRepository = CarRepository(carApi: carApi);
   runApp(
     App(
       authenticationRepository: authenticationRepository,
+      carRepository: carRepository,
     ),
   );
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:car_dealership/src/app_view.dart';
+import 'package:car_repository/car_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,14 +8,24 @@ class App extends StatelessWidget {
   const App({
     super.key,
     required AuthenticationRepository authenticationRepository,
-  }) : _authenticationRepository = authenticationRepository;
+    required CarRepository carRepository,
+  })  : _authenticationRepository = authenticationRepository,
+        _carRepository = carRepository;
 
   final AuthenticationRepository _authenticationRepository;
+  final CarRepository _carRepository;
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider.value(
-      value: _authenticationRepository,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(
+          value: _authenticationRepository,
+        ),
+        RepositoryProvider.value(
+          value: _carRepository,
+        ),
+      ],
       child: const AppView(),
     );
   }
