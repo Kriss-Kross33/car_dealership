bd346028aece79e7ddec59a1da184d181591d322
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-12-04 06:54:37 +0000

    make field empty string if null

--- packages/formz_input/lib/src/field.dart
+++ packages/formz_input/lib/src/field.dart
@@ -6,11 +6,12 @@ class Field extends FormzInput<String, FieldValidationError> {
   const Field.pure() : super.pure('');
   const Field.dirty([String value = '']) : super.dirty(value);
 
+  // static final regExp = RegExp(r'/^\s*(?:\d\s*){n}$/');
+
   @override
   FieldValidationError? validator(String? value) {
-    if (value != null) {
-      return value.isNotEmpty ? null : FieldValidationError.invalid;
-    }
-    return null;
+    if (value == null) value = '';
+    if (value.isNotEmpty) return null;
+    return FieldValidationError.invalid;
   }
 }
