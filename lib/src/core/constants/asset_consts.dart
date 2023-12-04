4e867f3b3d3b92d5bff70c86aa30d708764324b6
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-12-04 06:48:17 +0000

    add new assets path to `AssetConsts`

--- lib/src/core/constants/asset_consts.dart
+++ lib/src/core/constants/asset_consts.dart
@@ -1,32 +1,36 @@
 class AssetConsts {
   //* Icons
-  static const String notification = 'assets/images/notifications.png';
-  static const String filter2 = 'assets/images/filter2.png';
-  static const String search = 'assets/images/search.png';
-  static const String home = 'assets/images/home.png';
-  static const String fav = 'assets/images/fav.png';
-  static const String star = 'assets/images/star.png';
-  static const String car = 'assets/images/car.png';
-  static const String sellCar = 'assets/images/sell_car.png';
+  static const String notification = 'assets/icons/notifications.png';
+  static const String filter2 = 'assets/icons/filter2.png';
+  static const String search = 'assets/icons/search.png';
+  static const String home = 'assets/icons/home.png';
+  static const String fav = 'assets/icons/fav.png';
+  static const String star = 'assets/icons/star.png';
+  static const String car = 'assets/icons/car.png';
+  static const String sellCar = 'assets/icons/sell_car.png';
+  static const String sellCar2 = 'assets/icons/sell_car2.png';
 
-  static const String location = 'assets/images/location.png';
-  static const String gasoline = 'assets/images/gasoline.png';
-  static const String repair = 'assets/images/repair.png';
-  static const String rv = 'assets/images/rv.png';
-  static const String sedan = 'assets/images/sedan.png';
-  static const String tyre = 'assets/images/tyre.png';
-  static const String calendar = 'assets/images/calendar.png';
-  static const String engine = 'assets/images/engine.png';
-  static const String carRepair = 'assets/images/car_repair.png';
-  static const String ev = 'assets/images/ev.png';
-  static const String speedometer = 'assets/images/speedometer.png';
+  static const String location = 'assets/icons/location.png';
+  static const String gasoline = 'assets/icons/gasoline.png';
+  static const String repair = 'assets/icons/repair.png';
+  static const String rv = 'assets/icons/rv.png';
+  static const String sedan = 'assets/icons/sedan.png';
+  static const String tyre = 'assets/icons/tyre.png';
+  static const String calendar = 'assets/icons/calendar.png';
+  static const String engine = 'assets/icons/engine.png';
+  static const String carRepair = 'assets/icons/car_repair.png';
+  static const String carRepair2 = 'assets/icons/car_repair2.png';
+  static const String carRepair3 = 'assets/icons/car_repair3.png';
+  static const String carRepair4 = 'assets/icons/car_repair4.png';
+  static const String ev = 'assets/icons/ev.png';
+  static const String speedometer = 'assets/icons/speedometer.png';
 
-  static const String spanner2 = 'assets/images/spanner2.png';
+  static const String spanner2 = 'assets/icons/spanner2.png';
 
-  static const String person = 'assets/images/person.png';
-  static const String profile = 'assets/images/profile.png';
-  static const String mail = 'assets/images/mail.png';
-  static const String message = 'assets/images/message.png';
+  static const String person = 'assets/icons/person.png';
+  static const String profile = 'assets/icons/profile.png';
+  static const String mail = 'assets/icons/mail.png';
+  static const String message = 'assets/icons/message.png';
 
   static const String logo = 'assets/logos/logo.png';
   static const String logo2 = 'assets/logos/logo2.png';
